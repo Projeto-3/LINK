801b7e69c7c27250218ed13d6f0122a13088cb98
MaluArr <mluisaarruda@hotmail.com>
2023-11-01 22:20:30 -0300

    updating views

--- link/link_app/models.py
+++ link/link_app/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from enum import Enum
+from django.contrib.auth.models import User
 
 class Usuario(models.Model):
     nomeUsuario = models.CharField(max_length=50, null=False, blank=False) 
     email = models.EmailField()
+    senha = models.CharField(max_Lenght = 30, null = False, blank = False)
     cpf = models.CharField(max_length=11, null=False, primary_key=True)
     cidade = models.CharField(max_length=50, null=False)
     nascimentoData = models.DateField(max_length=10, null=False, blank=False)
-    # Como criar os tipos de usuário utilizando "enum"
+    tipoUsuario = models.IntegerField(max_length=1, null = False, blank=False)
+
+    def __str__(self):
+        return self.cpf
+    
+# Como criar os tipos de usuário utilizando "enum"
+class TipoDoUsuario(Enum):
+    usuarioComum = 1
+    voluntario = 2
+    embaixador = 3
+    donoDeProjeto = 4
+    Gerente = 5
+    
 
 class Projeto(models.Model):
     nomeProjeto = models.CharField(max_length=100, null=False, blank=False)
@@ -18,22 +33,34 @@ class Projeto(models.Model):
     demanda = models.TextField(max_length=1000, null=False, blank=False)
     idProjeto = models.CharField(primary_key=True)
 
+    def __str__(self):
+        return self.idProjeto
+
 class Doacao(models.Model):
     nomeDoador = models.ForeignKey(Usuario.nomeUsuario,on_delete=models.DO_NOTHING) # Ver qual é o melhor modo de deleção
     valorDoado = models.FloatField(max_length=50, null=False, blank=False)
     momentoDoacao = models.DateTimeField()
     idDoacao = models.CharField(primary_key=True)
 
+    def __str__(self):
+        return self.idDoacao
+
 class Demanda(models.Model):
-    idProjetoD = models.ForeignKey(Projeto.idProjeto, on_delete=models.DO_NOTHING) # Ver qual é o melhor modo de deleção
+    idProjetoDemanda = models.ForeignKey(Projeto.idProjeto, on_delete=models.DO_NOTHING) # Ver qual é o melhor modo de deleção
     idDemanda = models.CharField(primary_key=True)
     nomeDemanda = models.CharField(max_length=50, null=False, blank=False)
     descricaoDemanda = models.TextField(max_length=200, null=False, blank=False)
     contatoDemanda = models.CharField(max_length=50, null=False, blank=False)
 
+    def __str__(self):
+        return self.idProjetoDemanda
+    
 class Relatorio(models.Model):
     idRelatorio = models.CharField(primary_key=True)
-    idProjetoR = models.ForeignKey(Projeto.idProjeto, on_delete=models.DO_NOTHING) # Ver qual é o melhor modo de deleção
+    idProjetoRelatorio = models.ForeignKey(Projeto.idProjeto, on_delete=models.DO_NOTHING) # Ver qual é o melhor modo de deleção
     titulo = models.CharField(max_length=50, null=False, blank=False)
     descricao = models.TextField(max_length=15000, null=False, blank=False)
     dataRelatorio = models.DateTimeField()
+
+    def __str__(self):
+        return self.idProjetoRelatorio
--- link/link_app/views.py
+++ link/link_app/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Permission
+from link_app.models import Usuario, Perfil, Projeto, Demanda, Doacao, Relatorio
 
-# Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('') #Ve para onde redirecionar (MarketPlace)
+        
+    context = {'home': True}
+    return render(request, 'app/index.html', context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('') #home
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            senha = form.cleaned_data.get('senha')
+            user = authenticate(email=email, senha=senha)
+            if user is not None: 
+                login(request, user)
+                return redirect('') #home
+    else:
+        form = AuthenticationForm()
+    return render(request, 'app/login.html', {'form': form})
+
+
+def cadastro(request): 
+    if request.mathot == 'GET':
+        return render(request, ) #template
+    else: 
+        email == request.POST.get('email')
+        senha == request.POST.get('senha')
+        nomeUsuario == request.POST.get('nomeUsuario')
+        user = Usuario.object.filter(email = email).first()
+        #Verificar os outros atributos, especial o CPF válido ou não 
+    # cpf 
+    # cidade 
+    # nascimentoData 
+    # tipoUsuario
+
+
+#LÓGICA:
+# - VisualizarDados
+# - ArmazenamentoDados 
+# - AdmissãoGestor
+# - RelatarAtvProj
+# - CadastroDono
+# - EditInfoDono
+# - FiltroLoc
+# - AtribResp
+# - DivulgInfo
+
+def Visualizacao(request):
